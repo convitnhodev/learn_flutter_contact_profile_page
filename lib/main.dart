07b766da9db61961e3f6520b4be995c9e6370a63
convitnhodev <hungthai10a1@gmail.com>
2023-06-29 20:14:52 +0700

    addName

--- lib/main.dart
+++ lib/main.dart
@@ -28,16 +28,34 @@ class ContactProfilePage extends StatelessWidget {
          children: <Widget>[
            Column(
              children: <Widget>[
-               //Container for adding Profile picture
                Container(
-                 //Container is stretched horizontaly
                  width: double.infinity,
-                 //Image is fetched from url
                  child: Image.network(
 "https://scontent.fsgn2-7.fna.fbcdn.net/v/t39.30808-6/356206214_973274580684109_8362486192810212608_n.jpg?_nc_cat=100&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=i0WfKNKsnwwAX9DgWbh&_nc_ht=scontent.fsgn2-7.fna&oh=00_AfDJMhNXcIXQam210KVLy5o8iJIwpRQZzcff3Vbum_x-Uw&oe=64A22D97",
    			height: 250,
    			fit: BoxFit.cover,
  			),
+               ),
+               //NEW CODE: Adding Display Name
+               Container(
+                 height: 60,
+                 child: Row(
+                   //Aligning text to the start of the widget
+                   mainAxisAlignment: MainAxisAlignment.start,
+                   children: <Widget>[
+                     Padding(
+                       padding: const EdgeInsets.all(8.0),
+                       child: Text(
+                         "Thai Nguyen Viet Hung",
+                         style: TextStyle(fontSize: 30),
+                       ),
+                     ),
+                   ],
+                 ),
+               ),
+               //NEW CODE: Adding Divider below the display name
+               Divider(
+                 color: Colors.grey,
                ),
              ],
            ),
