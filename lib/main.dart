9cac002149f78f1e6dad13ab36c86bb903b60f01
convitnhodev <hungthai10a1@gmail.com>
2023-06-29 16:48:54 +0700

    addBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(ContactProfilePage());
 
 class ContactProfilePage extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: Scaffold(
+       appBar: AppBar(
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
-    );
-  }
+         //Adding background color to AppBar
+         backgroundColor: Colors.purple,
+         
+         //Adding back arrow for leading property.
+         //Back arrow appears at the top-left of AppBar
+         leading: Icon(
+           Icons.arrow_back,
+           color: Colors.black,
+         ),
+         actions: <Widget>[
+         
+           //Adding Star icon for favorting the contact.
+           //This IconButton responds to user's tap,
+           //and prints the message.
+           IconButton(
+             icon: Icon(Icons.star_border),
+             color: Colors.black,
+             onPressed: () {
+               print("Contact is starred");
+             },
+           ),
+         ],
+       ),
+     ),
+   );
+ }
 }
